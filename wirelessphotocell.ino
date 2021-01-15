cddc5674106bf3ada20d66ceca4ec7baeb5bf123
DiazGopar <jjdiaz@drimiun.com>
2021-01-15 15:56:31 +0000

    Included a continuous send loop to test

--- wirelessphotocell.ino
+++ wirelessphotocell.ino
@@ -23,7 +23,7 @@ uint8_t address[][6] = {"1Node", "2Node"};
 bool radioNumber = 1; // 0 uses address[0] to transmit, 1 uses address[1] to transmit
 
 // Used to control whether this node is sending or receiving
-bool role = false;  // true = TX role, false = RX role
+//bool role = false;  // true = TX role, false = RX role
 
 // For this example, we'll be using a payload containing
 // a single float number that will be incremented
@@ -58,13 +58,13 @@ void setup()
     }
     // Set the PA Level low 
     radio.setPALevel(RF24_PA_MAX);  // RF24_PA_MAX is default.
+
+    // Min speed for better range
+    radio.setDataRate(RF24_250KBPS);
     // save on transmission time by setting the radio to only transmit the
     // number of bytes we need 
     radio.setPayloadSize(sizeof(payload)); // float datatype occupies 4 bytes
-    // set the TX address of the RX node into the TX pipe
-    radio.openWritingPipe(address[radioNumber]);     // always uses pipe 0
-    // set the RX address of the TX node into a RX pipe
-    radio.openReadingPipe(1, address[!radioNumber]); // using pipe 1
+
     
     Serial.println(F("radio hardware initialized!!"));
     if(master)
@@ -73,6 +73,10 @@ void setup()
         display.clear();
         display.print(F("MSTR"));
         Serial.println(F("Node Configured in Master Mode"));
+        // set the TX address of the RX node into the TX pipe
+        radio.openWritingPipe(address[radioNumber]);     // always uses pipe 0
+        // set the RX address of the TX node into a RX pipe
+        radio.openReadingPipe(1, address[!radioNumber]); // using pipe 1
         radio.stopListening();  // put radio in TX mode
     }  
     else
@@ -81,6 +85,10 @@ void setup()
         display.clear();
         display.print(F("SLV"));
         Serial.println(F("Node Configured in Slave Mode"));
+        // set the TX address of the RX node into the TX pipe
+        radio.openWritingPipe(address[!radioNumber]);     // always uses pipe 0
+        // set the RX address of the TX node into a RX pipe
+        radio.openReadingPipe(1, address[radioNumber]); // using pipe 1
         radio.startListening(); // put radio in RX mode
     }
     display.blink(500);
@@ -94,5 +102,40 @@ void setup()
 
 void loop()
 {
+if (master) {
+    // This device is a TX node
 
+    unsigned long start_timer = micros();                    // start the timer
+    bool report = radio.write(&payload, sizeof(float));      // transmit & save the report
+    unsigned long end_timer = micros();                      // end the timer
+
+    if (report) {
+      Serial.print(F("Transmission successful! "));          // payload was delivered
+      Serial.print(F("Time to transmit = "));
+      Serial.print(end_timer - start_timer);                 // print the timer result
+      Serial.print(F(" us. Sent: "));
+      Serial.println(payload);                               // print payload sent
+      payload += 0.01;                                       // increment float payload
+    } else {
+      Serial.println(F("Transmission failed or timed out")); // payload was not delivered
+    }
+
+    // to make this example readable in the serial monitor
+    delay(1000);  // slow transmissions down by 1 second
+
+  } else {
+    // This device is a RX node
+
+    uint8_t pipe;
+    if (radio.available(&pipe)) {             // is there a payload? get the pipe number that recieved it
+      uint8_t bytes = radio.getPayloadSize(); // get the size of the payload
+      radio.read(&payload, bytes);            // fetch payload from FIFO
+      Serial.print(F("Received "));
+      Serial.print(bytes);                    // print the size of the payload
+      Serial.print(F(" bytes on pipe "));
+      Serial.print(pipe);                     // print the pipe number
+      Serial.print(F(": "));
+      Serial.println(payload);                // print the payload's value
+    }
+  } // role
 }
